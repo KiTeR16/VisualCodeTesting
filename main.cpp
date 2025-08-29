a4d4a6c5be6dfae3629347dae85511685dadf7ff
Piotr <piotr.zyhatynski@gmail.com>
2025-08-29 13:26:21 +0200

    main cpp new code

--- main.cpp
+++ main.cpp
@@ -2,5 +2,9 @@
 
 main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    
     return 0;
 }
